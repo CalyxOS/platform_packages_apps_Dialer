LineageOS/android_packages_apps_Dialer

Dialer: Move to SPN schema classes
Change-Id: I2cd3706f64bf45a2a6dd53caa9d5da48cf93ea28

java/com/android/dialer/helplines/HelplineItem.java
@@ -19,22 +19,22 @@ import android.content.res.Resources;
 
 import com.android.dialer.helplines.utils.HelplineUtils;
 
-import org.lineageos.lib.phone.SensitivePhoneNumberInfo;
+import org.lineageos.lib.phone.spn.Item;
 
 public class HelplineItem {
-    private final SensitivePhoneNumberInfo mInfo;
+    private final Item mItem;
     private final String mName;
 
-    public HelplineItem(Resources res, SensitivePhoneNumberInfo info, String countryIso) {
-        mInfo = info;
-        mName = HelplineUtils.getName(res, info, countryIso);
+    public HelplineItem(Resources res, Item item, String countryIso) {
+        mItem = item;
+        mName = HelplineUtils.getName(res, item, countryIso);
+    }
+
+    public Item getItem() {
+        return mItem;
     }
 
     public String getName() {
         return mName;
-    }
-
-    public String get(String key) {
-        return mInfo.get(key);
     }
 }

java/com/android/dialer/helplines/LoadHelplinesTask.java
@@ -23,8 +23,8 @@ import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.util.Log;
 
-import org.lineageos.lib.phone.SensitivePhoneNumberInfo;
 import org.lineageos.lib.phone.SensitivePhoneNumbers;
+import org.lineageos.lib.phone.spn.Item;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -72,11 +72,11 @@ public class LoadHelplinesTask extends AsyncTask<Void, Integer, List<HelplineIte
                 alreadyProcessedMccs.add(mcc);
 
                 SensitivePhoneNumbers spn = SensitivePhoneNumbers.getInstance();
-                ArrayList<SensitivePhoneNumberInfo> pns = spn.getSensitivePnInfosForMcc(mcc);
+                ArrayList<Item> pns = spn.getSensitivePnInfosForMcc(mcc);
                 int numPns = pns.size();
                 for (int i = 0; i < numPns; i++) {
-                    SensitivePhoneNumberInfo info = pns.get(i);
-                    helplineList.add(new HelplineItem(mResources, info,
+                    Item item = pns.get(i);
+                    helplineList.add(new HelplineItem(mResources, item,
                             addCountryCode ? subCountryIso : ""));
                     publishProgress(Math.round(i * 100 / numPns / subList.size()));
                 }

java/com/android/dialer/helplines/utils/HelplineUtils.java
@@ -20,7 +20,7 @@ import android.text.TextUtils;
 
 import com.android.dialer.R;
 import com.android.dialer.helplines.HelplineItem;
-import org.lineageos.lib.phone.SensitivePhoneNumberInfo;
+import org.lineageos.lib.phone.spn.Item;
 
 import java.util.ArrayList;
 
@@ -32,12 +32,12 @@ public class HelplineUtils {
     private static final String LANGUAGE_STR_FORMAT = "helpline_language_%s";
 
     /* Get the name of the helpline, fall back to the number if not given */
-    public static String getName(Resources res, SensitivePhoneNumberInfo info, String countryIso) {
-        if (info != null) {
-            String name = info.get("name");
+    public static String getName(Resources res, Item item, String countryIso) {
+        if (item != null) {
+            String name = item.getName();
             String displayName = !TextUtils.isEmpty(name)
                     ? getDisplayString(res, name, NAME_STR_FORMAT)
-                    : info.get("number");
+                    : item.getNumber();
             if (!TextUtils.isEmpty(countryIso)) {
                 return res.getString(R.string.helpline_name_format_country, displayName,
                         countryIso);
@@ -52,7 +52,7 @@ public class HelplineUtils {
     /* Split the given categories and translate them, fall back to "generic" if not given */
     public static String getCategories(Resources res, HelplineItem item) {
         if (item != null) {
-            String str = getDisplayString(res, item.get("categories"), CATEGORY_STR_FORMAT);
+            String str = getDisplayString(res, item.getItem().getCategories(), CATEGORY_STR_FORMAT);
             if (!TextUtils.isEmpty(str)) {
                 return str;
             }
@@ -64,7 +64,7 @@ public class HelplineUtils {
     /* Split and translate the given languages, return empty string if not given */
     public static String getLanguages(Resources res, HelplineItem item) {
         if (item != null) {
-            return getDisplayString(res, item.get("languages"), LANGUAGE_STR_FORMAT);
+            return getDisplayString(res, item.getItem().getLanguages(), LANGUAGE_STR_FORMAT);
         }
 
         return "";

CalyxOS/platform_packages_apps_Dialer

java/com/android/dialer/helplines/HelplineItem.java
@@ -19,22 +19,27 @@ import android.content.res.Resources;
 
 import com.android.dialer.helplines.utils.HelplineUtils;
 
-import org.calyxos.lib.phone.SensitivePhoneNumberInfo;
+import org.calyxos.lib.phone.spn.Item;
 
+/* When loading all the items we modify the name based on the subscription.
+ * Using the setter would modify it permanently, resulting in modifications on each load.
+ * Therefore we don't use Item directly but use this little helper class so the modified
+ * name can be stored
+ */
 public class HelplineItem {
-    private final SensitivePhoneNumberInfo mInfo;
+    private final Item mItem;
     private final String mName;
 
-    public HelplineItem(Resources res, SensitivePhoneNumberInfo info, String countryIso) {
-        mInfo = info;
-        mName = HelplineUtils.getName(res, info, countryIso);
+    public HelplineItem(Resources res, Item item, String countryIso) {
+        mItem = item;
+        mName = HelplineUtils.getName(res, item, countryIso);
+    }
+
+    public Item getItem() {
+        return mItem;
     }
 
     public String getName() {
         return mName;
-    }
-
-    public String get(String key) {
-        return mInfo.get(key);
     }
 }

java/com/android/dialer/helplines/LoadHelplinesTask.java
@@ -23,8 +23,8 @@ import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.util.Log;
 
-import org.calyxos.lib.phone.SensitivePhoneNumberInfo;
 import org.calyxos.lib.phone.SensitivePhoneNumbers;
+import org.calyxos.lib.phone.spn.Item;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -72,11 +72,11 @@ public class LoadHelplinesTask extends AsyncTask<Void, Integer, List<HelplineIte
                 alreadyProcessedMccs.add(mcc);
 
                 SensitivePhoneNumbers spn = SensitivePhoneNumbers.getInstance();
-                ArrayList<SensitivePhoneNumberInfo> pns = spn.getSensitivePnInfosForMcc(mcc);
+                ArrayList<Item> pns = spn.getSensitivePnInfosForMcc(mcc);
                 int numPns = pns.size();
                 for (int i = 0; i < numPns; i++) {
-                    SensitivePhoneNumberInfo info = pns.get(i);
-                    helplineList.add(new HelplineItem(mResources, info,
+                    Item item = pns.get(i);
+                    helplineList.add(new HelplineItem(mResources, item,
                             addCountryCode ? subCountryIso : ""));
                     publishProgress(Math.round(i * 100 / numPns / subList.size()));
                 }

java/com/android/dialer/helplines/utils/HelplineUtils.java
@@ -20,7 +20,8 @@ import android.text.TextUtils;
 
 import com.android.dialer.R;
 import com.android.dialer.helplines.HelplineItem;
-import org.calyxos.lib.phone.SensitivePhoneNumberInfo;
+
+import org.calyxos.lib.phone.spn.Item;
 
 import java.util.ArrayList;
 
@@ -32,12 +33,12 @@ public class HelplineUtils {
     private static final String LANGUAGE_STR_FORMAT = "helpline_language_%s";
 
     /* Get the name of the helpline, fall back to the number if not given */
-    public static String getName(Resources res, SensitivePhoneNumberInfo info, String countryIso) {
-        if (info != null) {
-            String name = info.get("name");
+    public static String getName(Resources res, Item item, String countryIso) {
+        if (item != null) {
+            String name = item.getName();
             String displayName = !TextUtils.isEmpty(name)
                     ? getDisplayString(res, name, NAME_STR_FORMAT)
-                    : info.get("number");
+                    : item.getNumber();
             if (!TextUtils.isEmpty(countryIso)) {
                 return res.getString(R.string.helpline_name_format_country, displayName,
                         countryIso);
@@ -52,7 +53,7 @@ public class HelplineUtils {
     /* Split the given categories and translate them, fall back to "generic" if not given */
     public static String getCategories(Resources res, HelplineItem item) {
         if (item != null) {
-            String str = getDisplayString(res, item.get("categories"), CATEGORY_STR_FORMAT);
+            String str = getDisplayString(res, item.getItem().getCategories(), CATEGORY_STR_FORMAT);
             if (!TextUtils.isEmpty(str)) {
                 return str;
             }
@@ -64,7 +65,7 @@ public class HelplineUtils {
     /* Split and translate the given languages, return empty string if not given */
     public static String getLanguages(Resources res, HelplineItem item) {
         if (item != null) {
-            return getDisplayString(res, item.get("languages"), LANGUAGE_STR_FORMAT);
+            return getDisplayString(res, item.getItem().getLanguages(), LANGUAGE_STR_FORMAT);
         }
 
         return "";