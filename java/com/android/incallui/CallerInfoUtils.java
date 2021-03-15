LineageOS/android_packages_apps_Dialer

Dialer: Hide sensitive numbers from incall screen
* In order to have less places where a sensitive number could be shown
  during usage, hide them from incall ui as well

Change-Id: I686f9a8fc09d62884344a2ddc949f12d0d5e1f7c

java/com/android/incallui/CallerInfoUtils.java
@@ -33,6 +33,10 @@ import com.android.dialer.util.PermissionsUtil;
 import com.android.incallui.call.DialerCall;
 import java.util.Arrays;
 
+import org.lineageos.lib.phone.SensitivePhoneNumbers;
+
+import static android.telephony.SubscriptionManager.INVALID_SUBSCRIPTION_ID;
+
 /** Utility methods for contact and caller info related functionality */
 public class CallerInfoUtils {
 
@@ -100,6 +104,7 @@ public class CallerInfoUtils {
         }
         number = modifyForSpecialCnapCases(context, info, number, info.numberPresentation);
       }
+      number = modifyPossibleSensitiveNumber(context, info, number);
       info.phoneNumber = number;
     }
 
@@ -138,6 +143,19 @@ public class CallerInfoUtils {
     CachedContactInfo cacheInfo = lookupService.buildCachedContactInfo(info);
     cacheInfo.setLookupKey(ci.lookupKeyOrNull);
     return cacheInfo;
+  }
+
+  static String modifySensitiveNumber(Context context, CallerInfo ci, String number) {
+    if (ci == null || number == null) {
+      return number;
+    }
+    boolean isSensitiveNumber = SensitivePhoneNumbers.getInstance().isSensitiveNumber(context,
+        number, INVALID_SUBSCRIPTION_ID);
+    if (isSensitiveNumber) {
+      number = context.getString(R.string.unknown);
+      ci.numberPresentation = TelecomManager.PRESENTATION_UNKNOWN;
+    }
+    return number;
   }
 
   /**

CalyxOS/platform_packages_apps_Dialer

java/com/android/incallui/CallerInfoUtils.java
@@ -16,6 +16,8 @@
 
 package com.android.incallui;
 
+import static android.telephony.SubscriptionManager.INVALID_SUBSCRIPTION_ID;
+
 import android.content.Context;
 import android.content.Loader;
 import android.content.Loader.OnLoadCompleteListener;
@@ -32,6 +34,8 @@ import com.android.dialer.phonenumberutil.PhoneNumberHelper;
 import com.android.dialer.util.PermissionsUtil;
 import com.android.incallui.call.DialerCall;
 import java.util.Arrays;
+
+import org.calyxos.lib.phone.SensitivePhoneNumbers;
 
 /** Utility methods for contact and caller info related functionality */
 public class CallerInfoUtils {
@@ -100,6 +104,7 @@ public class CallerInfoUtils {
         }
         number = modifyForSpecialCnapCases(context, info, number, info.numberPresentation);
       }
+      number = modifyPossibleSensitiveNumber(context, info, number);
       info.phoneNumber = number;
     }
 
@@ -138,6 +143,19 @@ public class CallerInfoUtils {
     CachedContactInfo cacheInfo = lookupService.buildCachedContactInfo(info);
     cacheInfo.setLookupKey(ci.lookupKeyOrNull);
     return cacheInfo;
+  }
+
+  static String modifyPossibleSensitiveNumber(Context context, CallerInfo ci, String number) {
+    if (ci == null || number == null) {
+      return number;
+    }
+    boolean isSensitiveNumber = SensitivePhoneNumbers.getInstance().isSensitiveNumber(context,
+        number, INVALID_SUBSCRIPTION_ID);
+    if (isSensitiveNumber) {
+      number = context.getString(R.string.unknown);
+      ci.numberPresentation = TelecomManager.PRESENTATION_UNKNOWN;
+    }
+    return number;
   }
 
   /**